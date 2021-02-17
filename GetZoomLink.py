a8b4dc781a241d4e88d7df4af9d42134267e0a45
Brandon Yao <ahoynodnarb@gmail.com>
2021-02-16 23:02:48 -0800

    Fixed hackyWorkaround, fixed label problems, condensed code, and fixed various index out of range errors

--- GetZoomLink.py
+++ GetZoomLink.py
@@ -10,6 +10,8 @@ from datetime import datetime
 from functools import partial
 from itertools import chain
 
+# {"links": [["https://pleasantonusd.zoom.us/j/91423967023", "https://pleasantonusd.zoom.us/j/98940695539", "https://pleasantonusd.zoom.us/j/92308403627"], ["https://pleasantonusd.zoom.us/j/97145648476?pwd=RGZTelZiMzJTa0sxelM5QVQvNk40UT09", "https://pleasantonusd.zoom.us/j/99537793707?pwd=Si8wVUZ1aFFBUUhabVpoME9YL0ZRQT09", "https://pleasantonusd.zoom.us/j/89495170511"], ["https://pleasantonusd.zoom.us/j/94553465337"]], "times": ["830", "950", "1110"], "manual": true}
+
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -24,7 +26,7 @@ class App(tk.Frame):
             self.links = []
             self.manual = tk.IntVar()
             self.entryLabel = tk.Label(
-                self, text="Link Monday", fg="black")
+                self, text="Link P1", fg="black")
             self.entryLabel.pack(side=tk.LEFT)
             self.entry = tk.Entry(self)
             self.entry.pack(side=tk.LEFT)
@@ -55,15 +57,11 @@ class App(tk.Frame):
                 config = json.load(f)
                 self.manual = config["manual"]
                 if(self.manual):
-                    lnk = config["links"][0]
-                    lnk.extend(config["links"][1])
-                    for i in range(len(lnk)//2):
-                        p1Button = tk.Button(self, text="Period " + str(i+i+1), command=partial(self.get_link, self.get_day_type(), "Period "+str(i*2+1), period=i, lst=0),
+                    lnk = config["links"]
+                    for i in range(len(lnk)-1):
+                        p1Button = tk.Button(self, text="Period " + str(i+1), command=partial(self.get_link, self.get_day_type(), "Period "+str(i+1), period=i),
                                              fg="black")
                         p1Button.pack(side=tk.LEFT, padx=21-len(lnk))
-                        p2Button = tk.Button(self, text="Period " + str(i+i+2), command=partial(self.get_link, self.get_day_type(), "Period "+str(i*2+2), period=i, lst=1),
-                                             fg="black")
-                        p2Button.pack(side=tk.LEFT, padx=21-len(lnk))
                     access = tk.Button(self, text="Access", command=lambda: self.get_link(self.get_day_type(), "Access"),
                                        fg="black")
                     access.pack(side=tk.LEFT, padx=21-len(lnk))
@@ -79,18 +77,18 @@ class App(tk.Frame):
         for i in items:
             i.destroy()
 
+    # {"links": [["https://pleasantonusd.zoom.us/j/91423967023", "https://pleasantonusd.zoom.us/j/98940695539", "https://pleasantonusd.zoom.us/j/92308403627"], ["https://pleasantonusd.zoom.us/j/97145648476?pwd=RGZTelZiMzJTa0sxelM5QVQvNk40UT09", "https://pleasantonusd.zoom.us/j/99537793707?pwd=Si8wVUZ1aFFBUUhabVpoME9YL0ZRQT09", "https://pleasantonusd.zoom.us/j/89495170511"], ["https://pleasantonusd.zoom.us/j/94553465337"]], "times": ["830", "950", "1110"], "manual": 1}
     def callback(self, isDay, day):
         with open("config.json", "w") as output:
-            days = ["Links Monday", "Links Tuesday", "Links Wednesday"]
-            if isDay:
-                self.links.append(self.entry.get().split(" "))
+            if(isDay):
+                self.links.append(self.entry.get())
             else:
                 self.times.append(self.entry.get())
             self.entry.delete(0, "end")
-            if day < 2:
+            if(day) < 5:
                 self.Submit.config(
                     command=lambda: self.callback(True, day+1))
-                self.entryLabel.config(text=days[day+1])
+                self.entryLabel.config(text="Link P" + str(day+2))
             else:
                 self.Submit.config(command=lambda: self.callback(False, day))
                 self.entryLabel.config(text="Times")
@@ -103,7 +101,8 @@ class App(tk.Frame):
         return ret
 
     def get_day_type(self):
-        if datetime.today().weekday() > 4:
+        return 3
+        if(datetime.today().weekday() > 4):
             return 1
         with open("dates.json") as f:
             data = json.load(f)
@@ -111,55 +110,47 @@ class App(tk.Frame):
             breakData = data["breaks"]
             today = datetime.now().strftime("%A %B %d %Y")
             day = today
-            if today in dateData[0]:
+            if(today in dateData[0]):
                 day = dateData[0][today].lower()
             today = self.convert_format(today, "%A %B %d %Y")
             for i in range(len(breakData)):
                 breakRange = list(breakData[i])
                 start = self.convert_format(breakRange[0], "%B %d %Y")
                 end = self.convert_format(breakRange[1], "%B %d %Y")
-                if start <= today <= end:
+                if(start <= today <= end):
                     day = breakData[i][end.strftime("%B %d %Y")].lower()
-            if day.find("no school") > 0:
+            if(day.find("no school") > 0):
                 return 1
-            elif day.find("minimum") > 0:
+            elif(day.find("minimum") > 0):
                 return 2
             return 3
 
-    def get_link(self, dayType, text=None, period=0, lst=2):
+    def get_link(self, dayType, text=None, period=6):
         now = datetime.today().now().strftime("%H%M")
         with open("config.json") as config:
             currentDay = datetime.today().weekday()
             elements = json.load(config)
             times = elements["times"]
             links = elements["links"]
-            if currentDay >= 3:
+            if(5 > currentDay >= 3):
                 currentDay = currentDay - 3
-            # takes closest number to current time as index
-            index = min(range(len(times)), key=lambda i: abs(
+            # takes closest number to current time as index from list
+            linkIndex = [[0, 2, 4], [1, 3, 5], [0]]
+            index = min(range(len(linkIndex[currentDay])), key=lambda i: abs(
                 int(times[i]) - int(now)))
-            currentPeriod = links[currentDay][index]
-            labels = ["No school today", links,
-                      f"{text} Link: \"{currentPeriod}\""]
-            if(currentDay == 2):
-                index = 0
+            currentPeriod = links[linkIndex[currentDay][index]]
+            if(currentDay == 2 and not self.manual):
+                currentPeriod = links[6]
                 text = "Access"
-            elif(self.manual):
+            if(self.manual):
                 index = period
-                currentPeriod = links[lst][index]
+                currentPeriod = links[index]
                 labelText = f"{text} Link: \"{currentPeriod}\""
             else:
+                text = "Period " + str(linkIndex[currentDay][index]+1)
+                labels = ["No school today", links,
+                          f"{text} Link: \"{currentPeriod}\""]
                 labelText = labels[dayType-1]
-            if(text == None):
-                # This is horrible and I probably shouldn't be using it but I'm sick of bug fixes
-                hackyWorkaround = [[], []]
-                arrIndex = 0
-                lnk = links[0]
-                lnk.extend(links[1])
-                for i in range(1, len(lnk)+1):
-                    hackyWorkaround[not arrIndex].append(i)
-                    arrIndex = not arrIndex
-                text = "Period " + str(hackyWorkaround[currentDay][index])
             self.clipboard_clear()
             self.clipboard_append(currentPeriod)
             self.linkLabel.config(text=labelText)
@@ -173,5 +164,5 @@ def main():
     root.mainloop()
 
 
-if __name__ == "__main__":
+if(__name__ == "__main__"):
     main()
